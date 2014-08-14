9b03368044f8ab7cd7ea8ec3230421ccb2a87308
Phil Chiu <pchiu@sandia.gov>
2014-08-14 14:08:40 -0600

    Removed spaces from blade element number output
    Changed the format string for the blade element number for a wake
    filament from '(I8)' to '(I0)' to remove leading spaces in the CSV
    output file.

--- src/WriteWakeElementData.f95
+++ src/WriteWakeElementData.f95
@@ -16,7 +16,7 @@ SUBROUTINE WriteWakeElementData()
     do wcount=1,NWakeInd 
         do tCount=1,tCountMax
             write(12,'(E13.7,",",$)') TimeN                             ! Normalized simulation time (t*Uinf/Rmax)                                   ! Timestep number
-            write(12,'(I8,",",$)') WakeLineInd(wcount)                  ! Blade element number that wake element originated from
+            write(12,'(I0,",",$)') WakeLineInd(wcount)                  ! Blade element number that wake element originated from
             write(12,'(E13.7,",",$)') X(tCount,WakeLineInd(wcount))     ! Wake element X position
             write(12,'(E13.7,",",$)') Y(tCount,WakeLineInd(wcount))     ! Wake element Y position
             write(12,'(E13.7,",",$)') Z(tCount,WakeLineInd(wcount))     ! Wake element Z position
