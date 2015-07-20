4d4dcef1e174c21c28cb4c8217c69274d0c08bcc
Phil Chiu <whophilchiu@gmail.com>
2015-07-20 17:41:46 -0600

    Cleaned up wallgeom.f95, added features
    - Cleaned up wallgeom.f95. Removed WP1(:) and WP2(:) which have no value
    for the general quadrilateral source panel case.
    
    - Added storage for NumNodes in WallType.
    
    - Added nodepoints_from_ij() subroutine which returns the four node
    points surrounding any particular element.

--- mod/wallgeom.f95
+++ mod/wallgeom.f95
@@ -13,13 +13,14 @@ module wallgeom
         integer           :: NumWP1                   ! Number of wall panels in the 1-direction
         integer           :: NumWP2                   ! Number of wall panels in the 2-direction
         integer           :: NumWP                    ! Number of wall panels (on this particular wall)
-        
+        integer           :: NumNodes                 ! Number of nodes (on this particula wall)
+
         real, allocatable :: WCPoints(:,:)            ! Panel center points (over radius)
         real, allocatable :: W1Vec(:,:)               ! Panel tangential vectors in the length direction
         real, allocatable :: W2Vec(:,:)               ! Panel tangential vectors in the width direction
         real, allocatable :: W3Vec(:,:)               ! Panel normal vectors
-        real, allocatable :: WP1(:)                   ! Panel lengths (over radius)
-        real, allocatable :: WP2(:)                   ! Panel widths (over radius)
+
+        real, allocatable :: pnodes(:,:)              ! Panel node locations (x,y,z)
         
         ! wall strength
         real, allocatable :: WSource(:,:)             ! Wall source density values (column vector) (non-dimensional, normalized by freestream velocity)
@@ -34,22 +35,23 @@ module wallgeom
 
     integer                     :: Nwalls             ! number of walls
     integer                     :: NumWP_total        ! total number of wall panels
+    integer                     :: NumNodes_total     ! total number of nodes
     type(WallType), allocatable :: Walls(:)
 
 contains
 
-    subroutine wall_cns(WallInd,NumWP)
+    subroutine wall_cns(WallInd,NumWP,NumNodes)
 
         ! Constructor for the arrays in this module
 
-        integer :: WallInd,NumWP
+        integer :: WallInd,NumWP,NumNodes
 
         allocate(Walls(WallInd)%WCPoints(NumWP,3))
         allocate(Walls(WallInd)%W1Vec(NumWP,3))
         allocate(Walls(WallInd)%W2Vec(NumWP,3))
         allocate(Walls(WallInd)%W3Vec(NumWP,3))
-        allocate(Walls(WallInd)%WP1(NumWP))
-        allocate(Walls(WallInd)%WP2(NumWP))
+
+        allocate(Walls(WallInd)%pnodes(NumNodes,3))
 
         allocate(Walls(WallInd)%WSource(NumWP,1))
 
@@ -59,6 +61,22 @@ contains
         allocate(Walls(WallInd)%ur(NumWP))
 
     end subroutine wall_cns
+
+
+    subroutine nodepoints_from_ij(Wall,i,j,p1,p2,p3,p4)
+        ! nodepoints_from_ij() : returns the coordinates of the four node points of the
+        !   quadrilateral source panel whose center is indexed by i,j
+
+        type(WallType), intent(in) :: Wall
+        integer, intent(in)        :: i, j
+        real, intent(out)          :: p1(3), p2(3), p3(3), p4(3)
+
+        p1 = Wall%pnodes(i  ,j  )
+        p2 = Wall%pnodes(i+1,j  )
+        p3 = Wall%pnodes(i+1,j+1)
+        p4 = Wall%pnodes(i  ,j+1)
+
+    end subroutine nodepoints_from_ij
 
 
     subroutine read_p3d_walls(WallMeshPath)
@@ -88,6 +106,7 @@ contains
 
         ! set initial value fo NumWP_total
         NumWP_total = 0
+        NumNodes_total = 0
 
         ! for each block (or each "wall panel")
         do m = 1,nblocks
@@ -101,14 +120,16 @@ contains
 
             ! add to the total number of wall panels
             NumWP_total = NumWP_total + npanels
+            NumNodes_total = NumNodes_total + nnodes
 
             ! set some WallType variables
-            Walls(m)%NumWP1 = ni(m)-1
-            Walls(m)%NumWP2 = nj(m)-1
-            Walls(m)%NumWP  = npanels
+            Walls(m)%NumWP1   = ni(m)-1
+            Walls(m)%NumWP2   = nj(m)-1
+            Walls(m)%NumWP    = npanels
+            Walls(m)%NumNodes = nnodes
 
             ! call the wall constructor subroutine (allocator for internal wall variables)
-            Call wall_cns(m,npanels)
+            Call wall_cns(m,npanels,nnodes)
 
             ! loop through grid coordinates
             idx = 1
@@ -122,20 +143,26 @@ contains
                     p4 = [x(i  ,j+1,k,m), y(i  ,j+1,k,m), z(i  ,j+1,k,m)]
 
                     Walls(m)%WCPoints(idx,1:3) = 0.25*(p1+p2+p3+p4)                                ! panel center
-
-                    Walls(m)%W1Vec(idx,1:3)    = p2-p1                                             ! panel x tangential vector
-                    Walls(m)%WP1(idx)          = sqrt(sum((p2-p1)**2))                             ! panel x length
-                    Walls(m)%W1Vec(idx,1:3)    = Walls(m)%W1Vec(idx,1:3)/Walls(m)%WP1(idx)         ! normalize
-
-                    Walls(m)%W2Vec(idx,1:3)    = p4-p1                                             ! panel y tangential vector, set so that panel normal will be in the domain inward direction
-                    Walls(m)%WP2(idx)          = sqrt(sum((p4-p1)**2))                             ! panel y length
-                    Walls(m)%W2Vec(idx,1:3)    = Walls(m)%W2Vec(idx,1:3)/Walls(m)%WP2(idx)         ! normalize
+                    Walls(m)%W1Vec(idx,1:3)    = (p4-p1)/sqrt(sum((p4-p1)**2))                     ! panel 1-tangential vector
+                    Walls(m)%W2Vec(idx,1:3)    = (p2-p1)/sqrt(sum((p2-p1)**2))                     ! panel 2-tangential vector
 
                     ! compute ! panel normal vector
                     Call cross(Walls(m)%W1Vec(idx,1),Walls(m)%W1Vec(idx,2),Walls(m)%W1Vec(idx,3),&
                                Walls(m)%W2Vec(idx,1),Walls(m)%W2Vec(idx,2),Walls(m)%W2Vec(idx,3),&
                                Walls(m)%W3Vec(idx,1),Walls(m)%W3Vec(idx,2),Walls(m)%W3Vec(idx,3))    
 
+                    idx = idx + 1
+                end do
+            end do
+
+            ! store node locations in an array (flattening)
+            idx = 1
+            do i=1,ni(m)
+                do j=1,nj(m)
+                    ! store node locations in array
+                    Walls(m)%pnodes(idx,1) = x(i,j,k,m)
+                    Walls(m)%pnodes(idx,2) = y(i,j,k,m)
+                    Walls(m)%pnodes(idx,3) = z(i,j,k,m)
                     idx = idx + 1
                 end do
             end do
