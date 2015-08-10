2d8c4f2783e04f7bc891d85e1c6296ea2ff42dbc
Phil Chiu <whophilchiu@gmail.com>
2015-08-10 10:55:10 -0600

    Changed ground plane output format to Tecplot (.tp)
    The ground plane output format now writes to a Tecplot unstructured
    mesh. This can be read directly into ParaView (although the SOLUTIONTIME)
    variable is currently unsupported.

--- src/WriteWallData.f95
+++ src/WriteWallData.f95
@@ -30,89 +30,71 @@ SUBROUTINE WriteWallData()
 
     end if
 
-    ! Write wake positions and velocity for each wake line on last rev
-    if (irev == nr) then      
+    if (irev == nr) then    
         OutCount=OutCount+1 
+    end if
 
-        if (GPFlag == 1) then
-            ! Output ground plane source density averaged over the last revolution
+    ! Output ground plane source density
+    if (GPFlag == 1) then
+        
+        !! write file to Tecplot Finite Element structured data format
+        write(nt_str,'(I0)') nt
+        filename=trim(FNBase)//'_WPData_'//trim(nt_str)//'.tp'
+        open(15, file=filename)
 
-            ! ! Average over last revolution
-            ! do i=1,NumWP                                                  
-            !     WSourceOut(i)=WSourceOut(i)+WSource(i,1)/real(nti)
-            ! end do
+        !! write header
+        write(15,*) 'TITLE="wall panel source strengths"'
+        write(15,*) 'VARIABLES=X, Y, Z, "sigma"'
 
-            ! ! Write on last iter
-            ! if (OutCount == nti) then
-            !     do i=1,NumWP     
-            !         write(14,'(E13.7,",",$)') WCPoints(i,1) 
-            !         write(14,'(E13.7,",",$)') WCPoints(i,2) 
-            !         write(14,'(E13.7,",",$)') WCPoints(i,3) 
-            !         ! Dont suppress carriage return on last column
-            !         write(14,'(E13.7)') WSourceOut(i)
-            !     end do
-            ! end if
+        ip_global=1
+        do iw=1,Nwalls
 
+            !! write each wall as a zone, specifying cellcentered data for sigma
+            write(15,'(A,I0,A,E13.7,A,I0,A,I0,A)') 'ZONE T="WP', iw, '", SOLUTIONTIME=', TimeN, ', N=', Walls(iw)%NumWPNodes, ', E=', Walls(iw)%NumWP, ', ET=QUADRILATERAL, F=FEBLOCK, VARLOCATION=([4]=CELLCENTERED) '
 
-            ! !! write file to Tecplot Finite Element structured data format
-            write(nt_str,'(I0)') nt
-            filename=trim(FNBase)//'_WPData_'//trim(nt_str)//'.tp'
-            open(15, file=filename)
-
-            !! write header
-            write(15,*) 'TITLE="wall panel source strengths"'
-            write(15,*) 'VARIABLES=X, Y, Z, "sigma"'
-
-            ip_global=1
-            do iw=1,Nwalls
-
-                !! write zone data, specifying cellcentered data for sigma
-                write(15,'(A,I0,A,E13.7,A,I0,A,I0,A)') 'ZONE T="WP', iw, '", SOLUTIONTIME=', TimeN, ', N=', Walls(iw)%NumWPNodes, ', E=', Walls(iw)%NumWP, ', ET=QUADRILATERAL, F=FEBLOCK, VARLOCATION=([4]=CELLCENTERED) '
-
-                ! write x of nodes
-                do i=1,Walls(iw)%NumWPNodes
-                    write(15,'(E13.7, " ",$)') Walls(iw)%pnodes(i,1)
-                end do
-                write(15,*) ""
-
-                ! write y of nodes
-                do i=1,Walls(iw)%NumWPNodes
-                    write(15,'(E13.7, " ",$)') Walls(iw)%pnodes(i,2)
-                end do
-                write(15,*) ""
-
-                ! write z of nodes
-                do i=1,Walls(iw)%NumWPNodes
-                    write(15,'(E13.7, " ",$)') Walls(iw)%pnodes(i,3)
-                end do
-                write(15,*) ""
-
-                ! write wall source strengths
-                do ip=ip_global,ip_global + Walls(iw)%NumWP - 1
-                    write(15,'(E13.7, " ",$)') WSource(ip, 1)
-                end do
-                write(15,*) ""
-
-                ! write node connectivity
-                do ip=1,Walls(iw)%NumWP
-                    ! get the local i and j
-                    call ip_local_to_ij_panel(Walls(iw),ip,i_local,j_local)
-
-                    ! write out the node numbers which define each quadrilateral panel
-                    ip1 = i_local   + (j_local-1)*(Walls(iw)%NumWP1+1)
-                    ip2 = i_local+1 + (j_local-1)*(Walls(iw)%NumWP1+1)
-                    ip3 = i_local+1 + (j_local  )*(Walls(iw)%NumWP1+1)
-                    ip4 = i_local   + (j_local  )*(Walls(iw)%NumWP1+1)
-
-                    write(15,'(I0," ",I0," ",I0," ",I0)') ip1,ip2,ip3,ip4
-                end do
-
-                ! advance the global panel index
-                ip_global = ip_global+Walls(iw)%NumWP
+            ! write x of nodes
+            do i=1,Walls(iw)%NumWPNodes
+                write(15,'(E13.7, " ",$)') Walls(iw)%pnodes(i,1)
             end do
-            close(15)
+            write(15,*) ""
 
-        end if
+            ! write y of nodes
+            do i=1,Walls(iw)%NumWPNodes
+                write(15,'(E13.7, " ",$)') Walls(iw)%pnodes(i,2)
+            end do
+            write(15,*) ""
+
+            ! write z of nodes
+            do i=1,Walls(iw)%NumWPNodes
+                write(15,'(E13.7, " ",$)') Walls(iw)%pnodes(i,3)
+            end do
+            write(15,*) ""
+
+            ! write wall source strengths
+            do ip=ip_global,ip_global + Walls(iw)%NumWP - 1
+                write(15,'(E13.7, " ",$)') WSource(ip, 1)
+            end do
+            write(15,*) ""
+
+            !! write node connectivity
+            do ip=1,Walls(iw)%NumWP
+                ! get the local i and j
+                call ip_local_to_ij_panel(Walls(iw),ip,i_local,j_local)
+
+                ! write out the node numbers which define each quadrilateral panel
+                ip1 = i_local   + (j_local-1)*(Walls(iw)%NumWP1+1)
+                ip2 = i_local+1 + (j_local-1)*(Walls(iw)%NumWP1+1)
+                ip3 = i_local+1 + (j_local  )*(Walls(iw)%NumWP1+1)
+                ip4 = i_local   + (j_local  )*(Walls(iw)%NumWP1+1)
+
+                write(15,'(I0," ",I0," ",I0," ",I0)') ip1,ip2,ip3,ip4
+            end do
+
+            ! advance the global panel index
+            ip_global = ip_global+Walls(iw)%NumWP
+        end do
+        close(15)
+
 
         if (FSFlag == 1) then
             ! Output wall tangent velocity and free surface height, dH/R=1/2*FnR^2*(1-(Ux@FS/UInf)^2)
