bee94a02ccd12807e6da54430018ffb8c88b4d0f
Phil Chiu <whophilchiu@gmail.com>
2020-06-09 21:29:24 -0600

    Use CMake to solve the problem with path separators
    This seemed easier than messing with compiler-specific macros

--- src/mod/pathseparator.f95
+++ src/mod/pathseparator.f95
@@ -7,13 +7,13 @@ module pathseparator
 contains
     
     subroutine get_path_separator()
-        ! get_path_separator() : Get the path delimiter ('/' for Linux, '\' for Windows)
-        character(len=99999) :: path
 
-        call get_environment_variable('PATH',path)
-
-        path_separator = path(1:1)
-
+#if defined(PATHSEP)
+        path_separator = PATHSEP
+#else
+        ! fallback
+        path_separator = '/'  
+#endif
     end subroutine get_path_separator
 
 end module pathseparator
